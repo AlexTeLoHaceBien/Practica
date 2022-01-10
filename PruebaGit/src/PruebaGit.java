b84252265eaebc642ba0da074564db4e2e13a7ff
AlexTeLoHaceBien <96080750+AlexTeLoHaceBien@users.noreply.github.com>
2022-01-10 19:55:07 +0100

    Update PruebaGit.java

--- PruebaGit/src/PruebaGit.java
+++ PruebaGit/src/PruebaGit.java
@@ -4,7 +4,7 @@ public class PruebaGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola, soy Git");
-		
+		System.out.println ("Version remote");
 		
 
 		
